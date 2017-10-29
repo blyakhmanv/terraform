96f7527e3bd0ec49a91741d70fa6ceb436f88ede
Vladimir <vb210979@mail.ru>
2017-10-29 18:38:02 +0300

    added security group and output var

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "server_port" {
+description = "The port the server will use for HTTP requests"
+  default = "8080"
+}
+
+
 provider "aws" {
 region = "us-east-1"
 }
@@ -5,7 +11,27 @@ region = "us-east-1"
 resource "aws_instance" "vbtest" {
 ami = "ami-40d28157"
 instance_type = "t2.micro"
-tags {
+user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p "${var.server_port}" &EOF
+
+  vpc_security_group_ids = ["${aws_security_group.vbgroup.id}"]
+  tags {
   Name = "VBtest"
 }
+}
+
+resource "aws_security_group" "vbgroup" {
+name = "terraform-example-instance"
+ingress {
+from_port = "${var.server_port}"
+to_port = "${var.server_port}"
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+output "public_ip" {
+value = "${aws_instance.vbtest.public_ip}"
 }
